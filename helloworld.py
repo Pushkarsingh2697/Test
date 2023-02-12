decf73f82194d1428b9a31209b46362296383c9c
Pushkar Singh <pushkarsingh@Pushkars-MacBook-Air.local>
2023-02-12 19:38:35 +0530

    first code

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+from asyncio import set_child_watcher
+
+
+print('hello, world!')
+print("Hello-world")
+print('Hello world''My first command in Python')
+print(7 - 5)
+print(3 - 1)
+print()
+print("The End", "this is my first code in python today is my first day in coding world.", "Thanks for your support")
+
+print('Good Evening', "All for my first code in Python ." "I am very happy to learn in python")
+print(7 - 5, 2 - 1) 
+print("hello team this is my new project")
+print("My Hovercraft is full of eels")
+print(6 * 7)
+print(5*5, 2-2, 2+2, 3+3)
+print(2+2)
+print(1-1+1)
